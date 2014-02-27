2c5a6e21461ab1976b4d516e398bfb8b9c27c26c
bezreyhan <bezreyhan@gmail.com>
2014-02-27 11:22:23 -0800

    add user model validations (username and password) and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :password, presence:true, length: {minimum: 6}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'validations' do
+
+    context "when username is not present" do 
+      it "the record is not valid" do 
+        user = User.new(username: '')
+        expect(user).not_to be_valid
+      end
+    end
+
+    context "when password is missing" do 
+      it "the record is not valid" do
+        user = User.new(username: "lorencio", password: '')
+        expect(user).not_to be_valid
+      end
+    end
+
+    context "when password is not at least 6 characters" do 
+      it "the record is not valid" do 
+        user = User.new(username: "lorencio", password: '12345')
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
