4a4796ab3c6d1140e0b391146c8ea3e9b51e5bcd
Ryan Holstein <ryan.holstein@gmail.com>
2014-03-04 09:30:37 -0800

    added action mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def index
     @users = User.all
   end
@@ -13,15 +13,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    
     @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "user was successfully created"
-      redirect_to users_path
-    else
-      render action: 'new'
-      flash[:error] = "aww shucks, user was not created!"
+   respond_to do |format|
+      if @user.save
+        UserMailer.welcome(@user.id).deliver
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
+
 
   def edit
     @user = User.find(params[:id])
