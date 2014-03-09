5b8d4d7be73ab2d3ffba1fed61bfc4cf1d577bdf
MarenWoodruff <wren403@gmail.com>
2014-03-08 21:19:05 -0800

    added an error partial to sign up page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,22 @@
 class UserMailer < ActionMailer::Base
   default from: "pairedups@gmail.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.welcome.subject
-  #
-  def welcome(user_id)
-   @user = User.find(user_id)
+    # Subject can be set in your I18n file at config/locales/en.yml
+    # with the following lookup:
+    #
+    #   en.user_mailer.welcome.subject
+    #
 
-    mail to: @user.email, subject: "Hey there"
-  end
+    def welcome(user_id)
+        @user = User.find(user_id)
+        #@url  = 'http://localhost:3000/auths/new'
 
-  # def tell_your_friends
-  #   mail to: "to@example.org", subject: "Tell your friend about our site"
-  # end
+        mail to: @user.email, subject: "Hey there"
+    end
+
+    # def tell_your_friends
+    # mail to: "to@example.org", subject: "Tell your friend about our site"
+    # end
 
 
 
